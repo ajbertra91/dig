48044df1b4bbd67d5e6a90b8fb90aaf4a88be954
ajbertra91 <ajbertra91@gmail.com>
2017-03-22 11:27:15 -0400

    Adds player sprite and setups the player

--- src/states/Main.js
+++ src/states/Main.js
@@ -2,21 +2,51 @@ import ExampleObject from 'objects/ExampleObject';
 
 class Main extends Phaser.State {
 
-	create() {
+    constructor(props) {
+        super(props);
+        this.player;
+        this.cursors;
+    }
 
-		//Enable Arcade Physics
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    create() {
 
-		//Set the games background colour
-		this.game.stage.backgroundColor = '#cecece';
+        //Enable Arcade Physics
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        // this.game.physics.arcade.enable(this.player);
+        //Set the games background colour
+        this.game.stage.backgroundColor = '#cecece';
 
-		//Example of including an object
-		//let exampleObject = new ExampleObject(this.game);
-	}
+        this.player = this.game.add.sprite(0, 0, 'player');
+        this.player.animations.add('up',    [0,1,2], 10, true);
+        this.player.animations.add('down',  [0,1,2], 10, true);
+        this.player.animations.add('left',  [3,4,5], 10, true);
+        this.player.animations.add('right', [6,7,8], 10, true);
+        // this.player.body.collideWorldBounds = true;
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.game.camera.follow(this.player);
 
-	update() {
-		
-	}
+        //Example of including an object
+        //let exampleObject = new ExampleObject(this.game);
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.x -= 4;
+            this.player.animations.play('left');
+        } else if (this.cursors.right.isDown) {
+            this.player.x += 4;
+            this.player.animations.play('right');
+        } else if (this.cursors.up.isDown) {
+            this.player.y -= 4;
+            this.player.animations.play('up');
+        } else if (this.cursors.down.isDown) {
+            this.player.y += 4;
+            this.player.animations.play('down');
+        } else {
+            this.player.animations.stop();
+            this.player.frame = 1;
+        }
+    }
 
 }
 
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -1,16 +1,17 @@
 class Preload extends Phaser.State {
 
-	preload() {
-		/* Preload required assets */
-		//this.game.load.image('myImage', 'assets/my-image.png');
-		//this.game.load.audio('myAudio', 'assets/my-audio.wav');
-		//this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
-	}
+    preload() {
+        /* Preload required assets */
+        //this.game.load.image('myImage', 'assets/my-image.png');
+        this.game.load.spritesheet('player', '/assets/dig_joshua.png', 48,48);
+        //this.game.load.audio('myAudio', 'assets/my-audio.wav');
+        //this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
+    }
 
-	create() {
-		//NOTE: Change to GameTitle if required
-		this.game.state.start("Main");
-	}
+    create() {
+        //NOTE: Change to GameTitle if required
+        this.game.state.start("Main");
+    }
 
 }
 
