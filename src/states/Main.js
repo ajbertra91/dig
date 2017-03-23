219e61457aec909ce641396819c0935a19ce2f71
ajbertra91 <ajbertra91@gmail.com>
2017-03-22 22:29:18 -0400

    adds some grass and trees...

--- src/states/Main.js
+++ src/states/Main.js
@@ -6,22 +6,42 @@ class Main extends Phaser.State {
         super(props);
         this.player;
         this.cursors;
+
+        this.ground;
+        this.treesGroup;
     }
 
     create() {
 
         //Enable Arcade Physics
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        // this.game.physics.arcade.enable(this.player);
         //Set the games background colour
-        this.game.stage.backgroundColor = '#cecece';
+        this.game.stage.backgroundColor = '#357736';
+
+        this.ground = this.game.add.tileSprite(0,0, this.game.width, this.game.height, 'groundTile1');
+        this.ground.scale.setTo(1,1);
+
+        this.treesGroup = this.game.add.group();
+        this.treesGroup.enableBody = true;
+        for (let i=0;i<25;i++) {
+            let x = this.game.rnd.integerInRange(32, this.game.width - 32);
+            let y = this.game.rnd.integerInRange(64, this.game.height - 64);
+            let tree = this.treesGroup.create(x,y,'tree1').scale.setTo(2,2);
+            this.game.physics.enable(tree);
+            tree.body.immovable = true;
+        }
 
         this.player = this.game.add.sprite(0, 0, 'player');
-        this.player.animations.add('up',    [0,1,2], 10, true);
+        this.player.animations.add('up',    [9], 10, true);
         this.player.animations.add('down',  [0,1,2], 10, true);
         this.player.animations.add('left',  [3,4,5], 10, true);
         this.player.animations.add('right', [6,7,8], 10, true);
-        // this.player.body.collideWorldBounds = true;
+        this.player.scale.setTo(1,1);
+
+        this.game.physics.arcade.enable(this.player);
+        this.player.body.collideWorldBounds = true;
+        this.game.physics.arcade.collide(this.player, this.treesGroup);
+
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.game.camera.follow(this.player);
 
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -4,6 +4,9 @@ class Preload extends Phaser.State {
         /* Preload required assets */
         //this.game.load.image('myImage', 'assets/my-image.png');
         this.game.load.spritesheet('player', '/assets/dig_joshua.png', 48,48);
+        this.game.load.image('groundTile1', '/assets/ground_tile1.png');
+        this.game.load.image('tree1', '/assets/tree1.png');
+
         //this.game.load.audio('myAudio', 'assets/my-audio.wav');
         //this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
     }
